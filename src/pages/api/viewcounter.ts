76ae6ce2f673aef6e678551451ebed1dbd5ac767
Nicolas Renault <contact.nicolasrenault@gmail.com>
2026-06-14 18:20:03 +0200

    Add viewcounter using Endpoint

---
+++ src/pages/api/viewcounter.ts
@@ -0,0 +1,38 @@
+import fs from "node:fs";
+
+const filePath = "./public/viewcounter.txt";
+
+export async function GET() {
+    console.log(Date.now());
+
+    try {
+        if (!fs.existsSync(filePath)) {
+            console.error("File doesn't exist");
+            throw new Error("File doesn't exist");
+        } else {
+            const fileData = fs.readFileSync(filePath, "utf-8");
+            let numberOfViews: number = Number(fileData) || 0;
+            numberOfViews++;
+            
+            fs.writeFileSync(filePath, String(numberOfViews));
+
+            return new Response(
+                JSON.stringify({
+                    message: numberOfViews
+                }),
+                {status: 200}
+            );
+        }
+    }
+    catch(error){
+        console.error("catch: " + error);
+        return new Response(
+            JSON.stringify({
+                message: error
+            }),
+            {status: 500}
+        );
+    }
+
+    
+}
